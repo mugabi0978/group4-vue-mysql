717590a1dcd30ed7d44ee3719299536958082ca1
Sam <powellorenz@gmail.com>
2023-07-20 19:49:31 +0300

    Pre-Release appointments 1.1

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    HOST: "52.90.135.103",
+    // HOST: "52.90.135.103",
+    HOST: "localhost",
     USER: "schoolpe_group1_user",
     PASSWORD: "9aFkIR5m3CtU#2wK",
     DB: "schoolpe_group1",
@@ -12,7 +13,7 @@ module.exports = {
         // maximum time, in milliseconds, that the db pool tries to connect before it throws an error
         acquire: 30000,
         // maximum time, in milliseconds, that a connection can be idle before being released
-        idle: 10000
+        idle: 100000000
 
     }
 }
